799bdf78511abff753913875b0f30e0801bfe96a
EnZo-Z21 <your.email@example.com>
2026-02-10 11:26:22 -0500

    Finished Intial Start Screen Layout

--- quiz_app_sp26/lib/start_screen.dart
+++ quiz_app_sp26/lib/start_screen.dart
@@ -4,15 +4,33 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(BuildContext context){
-    //TODO: implement build
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [
           Color.fromARGB(255, 63, 8, 165),
           Color.fromARGB(255, 12, 1, 39)
-        ])
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        )
       ),
-      child: Center(child: Text("Start Quiz"),),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/quiz-logo.png', width: 280),
+          
+            Text("Challenge yourself on this quiz!", style: TextStyle(color:Colors.white, fontSize:28)
+            ),
+            const SizedBox(height: 30),
+            OutlinedButton(
+              onPressed: () {}, 
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,),
+              child: Text("Start Quiz"),
+            ),
+          ],
+        ),
     );
   }
 }

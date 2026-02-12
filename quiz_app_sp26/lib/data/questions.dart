96fec753d98ddc80fc14c73049aef81b0e26de4c
EnZo-Z21 <your.email@example.com>
2026-02-12 11:33:51 -0500

    Created question model and added questions

---
+++ quiz_app_sp26/lib/data/questions.dart
@@ -0,0 +1,9 @@
+import 'package:quiz_app_sp26/models/quiz_question.dart';
+const questions = [
+  QuizQuestion('Who won the World Cup in 1986', ['Argentina', 'Germany', 'Italy', 'France']),
+  QuizQuestion('Who won the World Cup in 1998', ['France', 'Germany', 'Italy', 'Argentina']),
+  QuizQuestion('Who won the World Cup in 2014', ['Germany', 'Argentina', 'Italy', 'France']),
+  QuizQuestion('Who won the Champions League in 2020', ['Bayern Munich', 'PSG', 'Barcelona', 'Real Madrid']),
+  QuizQuestion('Who won the Champions League in 2015', ['Barcelona', 'Real Madrid', 'Bayern Munich', 'PSG']),
+  QuizQuestion('Who won the Champions League in 2025', ['Real Madrid', 'Barcelona', 'Bayern Munich', 'PSG'])
+];

154137c9689f471457c93a46de78509af3f33bc7
EnZo-Z21 <your.email@example.com>
2026-03-26 11:46:16 -0400

    Made dummy data in expenses

--- expense_tracker_sp26/lib/expenses.dart
+++ expense_tracker_sp26/lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_sp26/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -6,9 +7,13 @@ class Expenses extends StatefulWidget {
   @override
   State<Expenses> createState() {
     return _ExpensesState();
-}
+  }
 }
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Ginos Pizza', amount: 25.00, date: DateTime.now(), category: Category.food),
+    Expense(title: 'Train Ticket to MA', amount: 15.25, date: DateTime.now(), category: Category.travel),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(

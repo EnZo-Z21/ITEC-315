07a7f6e5c900b262f1b799ef7019073837f19f10
EnZo-Z21 <your.email@example.com>
2026-04-16 11:32:26 -0400

    Finished Dark Mode

--- expense_tracker_sp26/lib/main.dart
+++ expense_tracker_sp26/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker_sp26/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+var kDarkColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 99, 125), brightness: Brightness.dark);
 
 void main() {
   runApp( MaterialApp(
@@ -24,11 +25,23 @@ void main() {
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-            foregroundColor: kColorScheme.onPrimaryContainer
+            backgroundColor: kColorScheme.primaryContainer
           )
         )
     ),
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: CardThemeData(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer
+          )
+        )
+    ),
+    themeMode: ThemeMode.system,
     home: Expenses()
   ));
 }
